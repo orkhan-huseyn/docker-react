1ac476380e879cfb297c916fa736ea8b59025eba
Orkhan Huseynli <orkhan.huseyn@outlook.com>
2020-03-16 23:27:28 +0400

    tiny changes

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edit <code>src/App.js</code> and save to reload and enjoy.
         </p>
         <a
           className="App-link"
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          I was changed on feature branch
+          Learn React
         </a>
       </header>
     </div>
